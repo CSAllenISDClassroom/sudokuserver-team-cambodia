e0072d63b931f64f159e8877ebd50af5d172319e
SheLikesPigs <nidhi.pabbathi@student.allenisd.org>
2021-09-14 14:58:03 -0500

    changes added

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,7 +2,7 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "It works! :D"
+        return "It works!! :D"
     }
 
     app.get("hello") { req -> String in
