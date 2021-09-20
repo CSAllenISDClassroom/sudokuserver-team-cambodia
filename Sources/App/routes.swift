e246fc75daf2de91d98504b73ad9f7d51f97fcc6
Ricky Das <ankit.das@student.allenisd.org>
2021-09-20 14:53:20 -0500

    Created a boardID generator

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,17 @@
 import Vapor
 
+var boardID = 0
+
+func updateBoardID() {
+    boardID += 1
+}
+
 func routes(_ app: Application) throws {
     app.get { req in
         return "It works! :D"
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("games") { req -> String in
+        return "\(boardID)"
     }
 }
