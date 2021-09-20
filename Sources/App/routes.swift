836770688a51f04d563d0588a8d3c054f1ef8ad3
SheLikesPigs <nidhi.pabbathi@student.allenisd.org>
2021-09-20 14:39:29 -0500

    updated routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,33 @@
 import Vapor
 
+struct Background : Content {
+    var action : String
+    var payload : String
+    var response : String
+    var statusCode : String
+}
+
+struct SudokuBoard: Content {
+    var board: String
+}
+
 func routes(_ app: Application) throws {
+    var board : String
     app.get { req in
-        return "It works! :D"
+        return "It works!"
     }
 
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+
+    //    app.post("games") { req in
+    
+    //    }
+
+    app.get("games", ":id", "cells") { req -> SudokuBoard in
+        let board = SudokuBoard(board: generateBoard())
+        return board
+    }
+    
 }
