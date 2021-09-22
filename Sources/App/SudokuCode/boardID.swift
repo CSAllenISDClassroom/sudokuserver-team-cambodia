6a0bf30ef3a44102dcadcd4da84153451e5d229a
Ricky Das <ankit.das@student.allenisd.org>
2021-09-22 15:18:24 -0500

    Updated the boardID class.

--- Sources/App/SudokuCode/boardID.swift
+++ Sources/App/SudokuCode/boardID.swift
@@ -1,4 +1,8 @@
 class boardID {
-    static var latestBoardID
+    private static var latestBoardID = -1
     
+    public func updateBoardID() -> Int {
+        boardID.latestBoardID += 1
+        return boardID.latestBoardID
+    }
 }
