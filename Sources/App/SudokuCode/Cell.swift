b154ed7afb2dda4c69c26a6c8ab6239442137cd2
Ricky Das <ankit.das@student.allenisd.org>
2021-09-23 20:27:16 -0500

    Updated Cell and Row files

--- Sources/App/SudokuCode/Cell.swift
+++ Sources/App/SudokuCode/Cell.swift
@@ -1,3 +1,12 @@
 class Cell {
-    var valueInCell : Int
+    public var valueInCell : Int
+    private let xCoordinate : Int
+    private let yCoordinate : Int
+    
+    init(value: Int, xCoordinate: Int, yCoordinate: Int) {
+        self.valueInCell = value
+        self.xCoordinate = xCoordinate
+        self.yCoordinate = yCoordinate
+    }
+    
 }
--- Sources/App/SudokuCode/Row.swift
+++ Sources/App/SudokuCode/Row.swift
@@ -1,4 +1,7 @@
 class Row {
     // A Row is a horizontal collection of cells, 9 cells long
-    var cells = [Cells]()
+    public var cells : [Cell]
+    init(cellsArray: Array) {
+        precondition(cellsArray.count == 9, "An attempt was made to initialize a row without 9 cells")
+    }
 }
