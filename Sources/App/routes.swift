cef3b2f7595f4eba4a0691e1d841e6b1defb65d1
Vaishnavi Susarla <susarla.vaishnavi@gmail.com>
2021-09-24 05:22:20 -0500

    I finished commenting on the file routes.swift.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -67,22 +67,22 @@ func routes(_ app: Application) throws {
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     app.put("games", ":id", "cells", ":boxIndex", ":cellIndex") { req -> ResponseData in
 
-        //
+        //The variable id requests to retrieve a particular boardID for a the specific game being played by the client
         let id = req.parameters.get("id") ?? -1
 
-        //
+        //The boardArray has the cell values of a completed board with the boxIndex and the cellIndex specifying the location of a requested cell
         let boardArray = sudokuIDs[id]?.squaresInfo ?? Board().squaresInfo
         let boxIndex = req.parameters.get("boxIndex") ?? -1
         let cellIndex = req.parameters.get("cellIndex") ?? -1
 
-        //
+        //This loop goes through the contents of boardArray to find the value of a specific cell that matches the requested boxIndex and cellIndex
         for square in boardArray {
             if square.box == boxIndex && square.cellIndex == cellIndex {
                 square.number = -1 //what do we set the number to??
             }
         }
 
-        //
+        //This return statement communicates to the client whether or not their requested value has been placed in the boxIndex and cellIndex
         return ResponseData(action: "Placed value in boxIndex, cellIndex", payload: "number??", response: "Nothing", statusCode: "204 No content") //number??
     }
 }
