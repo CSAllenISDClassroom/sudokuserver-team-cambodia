30e1973d6680a0979a6cbab280b67f0206b35d22
Vaishnavi Susarla <susarla.vaishnavi@gmail.com>
2021-09-24 04:50:26 -0500

    I finished commenting the file Cell.swift

--- Sources/App/SudokuCode/Cell.swift
+++ Sources/App/SudokuCode/Cell.swift
@@ -1,9 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//This class maintains the variables necessary for associating the location of a cell with its value inside
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 class Cell {
+
+    //Both the xCoordinate and the yCoordinate are private to ensure that the valueInCell is the only variale change at a certain cell location
     public var valueInCell : Int
     private let xCoordinate: Int
     private let yCoordinate: Int
 
     init(valueInCell: Int, xCoordinate: Int, yCoordinate: Int) {
+
+        //Here the three variables have been initialized
         self.valueInCell = valueInCell
         self.xCoordinate = xCoordinate
         self.yCoordinate = yCoordinate
