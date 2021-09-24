54eefd481295c576b55125f9670c9986da72d10d
Vaishnavi Susarla <susarla.vaishnavi@gmail.com>
2021-09-23 21:31:14 -0500

    I started the process of going through the code and adding in comments in order to clarify and organize the code to improve the viewer's understanding.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,10 +1,16 @@
 import Vapor
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//The struct SudokuBoard includes the representation of the board as a string and the boardID as an Integer as well
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 struct SudokuBoard: Content {
     var board: String
     var boardID: Int
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//The struct ResponseData maintains the variables action, payload, response, and statusCode
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 struct ResponseData : Content{
     
     var action : String
@@ -14,42 +20,69 @@ struct ResponseData : Content{
 
 }
 
+
 var sudokuIDs = [Int:Board]() //class of boardsData
 
 let latestBoardID = boardID()
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//This functions details the GET, PUT, and POST commands for the client to use to send requests to the server via an API
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 func routes(_ app: Application) throws {
+    //making peace with vapor
     app.get { req in
         return "vapor my beloved"
     }
 
-    app.post("games") { req -> ResponseData in 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //The POST command below creates a new game and also creates a board ID associated with the specific game created.
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    app.post("games") { req -> ResponseData in
+
+        //The variable board is of the type Board, which creates a new game, while id is the type boardID and is the new board ID for the new game
         let board = Board()
         let id = latestBoardID.updateBoardID()
 
         sudokuIDs[id] = board
-        
+
+        //This return statement is of the type ResponseData and communicates to the client of the new game, boardID, and the server's statusCode
         return ResponseData(action: "Created Game", payload: "None", response: "\(id)", statusCode: "201 Created")  
     }
-    
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     app.get("games", ":id", "cells") { req -> ResponseData in
+
+        //
         let id = req.parameters.get("id") ?? -1
         let cells = sudokuIDs[id] ?? Board()
+
+        //
         return ResponseData(action: "None", payload: "None", response: cells.finalBoard, statusCode: "200 OK")
     }
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     app.put("games", ":id", "cells", ":boxIndex", ":cellIndex") { req -> ResponseData in
+
+        //
         let id = req.parameters.get("id") ?? -1
+
+        //
         let boardArray = sudokuIDs[id]?.squaresInfo ?? Board().squaresInfo
         let boxIndex = req.parameters.get("boxIndex") ?? -1
         let cellIndex = req.parameters.get("cellIndex") ?? -1
 
+        //
         for square in boardArray {
             if square.box == boxIndex && square.cellIndex == cellIndex {
                 square.number = -1 //what do we set the number to??
             }
         }
-        
+
+        //
         return ResponseData(action: "Placed value in boxIndex, cellIndex", payload: "number??", response: "Nothing", statusCode: "204 No content") //number??
     }
 }
