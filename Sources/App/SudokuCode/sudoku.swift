b9c773df403046be7a9b333f89d994007f57b9f3
Ricky Das <ankit.das@student.allenisd.org>
2021-09-27 21:05:11 -0500

    Created a GENIUS system to identify which box a number is in.

--- Sources/App/SudokuCode/sudoku.swift
+++ Sources/App/SudokuCode/sudoku.swift
@@ -2,19 +2,24 @@
 //The class below creates a new board by randomly choosing values from 1...9 in order to create a solvable sudoku board
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
 class sudokuBoard {
     
     public var sudokuBoardRows = [[Int]]()
     public var sudokuBoardColumns = [[Int]]()
     public var sudokuBoardBoxes = [[Int]]()
 
+
     //When this function is invoked, a new board will be generated 
     public func generateNewBoard() {
+
+        createNewEmptyArrays()
         
         var availableNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         var firstArray = [Int]()
         //This loop will randomly select a number from 1...9 as the potentialNumber to place from an array of availableNumbers
         for row in 0...8 {
+            
             for column in 0...8 {
                 var randomIndex = 0
                 var potentialValue = 0
@@ -23,41 +28,42 @@ class sudokuBoard {
                 randomIndex = Int.random(in: 0 ..< availableNumbers.count)
                 potentialValue = availableNumbers[randomIndex]
                 firstArray.append(potentialValue)
-                availableNumbers.remove(at: randomIndex)
+                //                availableNumbers.remove(at: randomIndex)
+
+                appendValueToArrays(value:potentialValue, rowIndex:row, columnIndex: column)
 
             }
         }
-        
-        func isValueInColumn(columnNumber:Int, value:Int)-> Bool {
-            var isValueInColumn = false 
-            for row in sudokuBoard {
-                if row[columnNumber] == value  {
-                    isValueInColumn == true
-                } 
+
+        func createEmptyArrays() {
+            for i in 0...8 {
+                var row = [Int]()
+                sudokuBoardRows.append(row)
             }
-            return isValueInColumn 
+
+            for i in 0...8 {
+                var column = [Int]()
+                sudokuBoardColumns.append(column)
+            }
+
+            for i in 0...8 {
+                var box = [Int]()
+                sudokuBoardBoxes.append(box)
+            }
+            
         }
-        func isValueInBox(topLeftColumnIndexOfBox:Int,firstRowIndexOfBox:Int,value:Int) -> Bool {
-            var boxValues :[Int] = []
-            var boxRows : [[Int]] = []
-            var isValueInBox = false 
-           
-            boxRows.append(sudokuBoard[firstRowIndexOfBox],sudokuBoard[firstRowIndexOfBox+1],sudokuBoard[firstRowIndexOfBox+2])
 
-            for row in boxRows {
-                boxValues.append(row[topLeftColumnIndexOfBox],row[topLeftColumnIndexOfBox+1],row[topLeftColumnIndexOfBox+2])
-                
-            }
+        func appendValueToArrays(value: Int, rowIndex: Int, columnIndex: Int) {
 
-            for boxValue in boxValues {
-                if boxValue == value{
-                    isValueInBox = true
-                }
-            }
-            return isValueInBox 
-                
-           
-    }
+            sudokuBoardRows[rowIndex].append(value)
+            sudokuBoardColumns[columnIndex].append(value)
+
+            let boxRow = rowIndex / 3
+            let boxColumn = columnIndex / 3
+            let boxIndex = (boxRow * 3) + boxColumn
+
+            precondition(boxIndex >= 0 && boxIndex <= 8, "boxIndex \(boxIndex) is out of bounds")
+            sudokuBoardBoxes[boxIndex].append(value)
+        }
     }
 }
-
