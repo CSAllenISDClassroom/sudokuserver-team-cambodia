cf36789570810674f8ec3638732c25581669de5d
Ricky Das <ankit.das@student.allenisd.org>
2021-09-27 20:14:07 -0500

    Created a nested for loop to the generateSudokuBoard Function

--- Sources/App/SudokuCode/sudoku.swift
+++ Sources/App/SudokuCode/sudoku.swift
@@ -1,24 +1,31 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //The class below creates a new board by randomly choosing values from 1...9 in order to create a solvable sudoku board
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class sudokuBoard {
 
-    //When this function is invoked, a new game will be generated 
+class sudokuBoard {
+    
+    public var sudokuBoardRows = [[Int]]()
+    public var sudokuBoardColumns = [[Int]]()
+    public var sudokuBoardBoxes = [[Int]]()
+
+    //When this function is invoked, a new board will be generated 
     public func generateNewBoard() {
         
         var availableNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         var firstArray = [Int]()
         //This loop will randomly select a number from 1...9 as the potentialNumber to place from an array of availableNumbers
-        for _ in 0...8 {
-            var randomIndex = 0
-            var potentialValue = 0
-            //let numberConflicts = false
-            
-            randomIndex = Int.random(in: 0 ... availableNumbers.count - 1)
-            potentialValue = availableNumbers[randomIndex]
-            firstArray.append(potentialValue)
-            availableNumbers.remove(at: randomIndex)
+        for row in 0...8 {
+            for column in 0...8 {
+                var randomIndex = 0
+                var potentialValue = 0
+                //let numberConflicts = false
+                
+                randomIndex = Int.random(in: 0 ..< availableNumbers.count)
+                potentialValue = availableNumbers[randomIndex]
+                firstArray.append(potentialValue)
+                availableNumbers.remove(at: randomIndex)
 
+            }
         }
         
         func isValueInColumn(columnNumber:Int, value:Int)-> Bool {
