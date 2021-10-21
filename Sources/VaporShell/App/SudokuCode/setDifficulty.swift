04ede4527fe35d062304751aa3a751b4a2102778
Vaishnavi Susarla <susarla.vaishnavi@gmail.com>
2021-10-20 21:36:52 -0500

    I started making the setDifficulty function in order to remove numbers from the solved board.

--- Sources/VaporShell/App/SudokuCode/setDifficulty.swift
+++ Sources/VaporShell/App/SudokuCode/setDifficulty.swift
@@ -3,4 +3,12 @@ import Vapor
 import Foundation
 
 func setDifficulty(difficulty: String) {
-    
+    func remove() {
+        var completeBoard = String(printBoard())
+        var string = ""
+        for x in completeBoard {
+            string.append(x)
+        }
+        print(string)
+    }
+    remove()
