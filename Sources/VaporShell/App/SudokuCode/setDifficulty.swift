f93ad9e97426e9872fac8b2cec6245ea6d26e247
Anamika Srivastav <anamika.srivastav@student.allenisd.com>
2021-10-20 21:15:07 -0500

    added different ammount of numbers to be removed for each difficulty

--- Sources/VaporShell/App/SudokuCode/setDifficulty.swift
+++ Sources/VaporShell/App/SudokuCode/setDifficulty.swift
@@ -2,17 +2,34 @@ import sudoku
 import Vapor
 import Foundation
 
+func removeNumbers(numbersToRemove:Int) {
+//removes the numbers 
+    for number in  1...numbersToRemove {
+        //removes a number at a random box Index adn cell index
+        removeNumberFromSudokuBoard(boxIndex:Int.random(in(0..<9)), cellIndex: Int.random(in(0..<9))) 
+        
+    }
+
+} 
 func setDifficulty(difficulty: String) {
 
     if difficulty == "easy" {
-       
+        removeNumbers(numbersToRemove: 5)      
     }
     if difficulty == "medium" {
+         removeNumbers(numbersToRemove: 10)
     }
     if difficulty == "hard" {
+         removeNumbers(numbersToRemove: 15)
     }
     if difficulty == "hell" {
+         removeNumbers(numbersToRemove: 20)
     }
     
     
 }
+
+func removeNumberFromSucokuBoard(boxIndex:Int,cellIndex: Int) {
+
+
+} 
