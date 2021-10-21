f5e430d3ebd69a6dc0f47b0ab01b809e82947a42
Anamika Srivastav <anamika.srivastav@student.allenisd.com>
2021-10-20 21:01:10 -0500

    updates set difficulty function

--- Sources/VaporShell/App/SudokuCode/setDifficulty.swift
+++ Sources/VaporShell/App/SudokuCode/setDifficulty.swift
@@ -3,4 +3,16 @@ import Vapor
 import Foundation
 
 func setDifficulty(difficulty: String) {
+
+    if difficulty == "easy" {
+       
+    }
+    if difficulty == "medium" {
+    }
+    if difficulty == "hard" {
+    }
+    if difficulty == "hell" {
+    }
     
+    
+}
